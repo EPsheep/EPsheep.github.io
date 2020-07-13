04853ec1d34963cc5ae839bbdefe1e492804583f
hurong <epsmail@126.com>
2020-07-13 18:01:14 +0800

    change config

--- build/build.php
+++ build/build.php
@@ -14,9 +14,12 @@ $config = [
     "email"                     => "epsmail@126.com",
     "description"               => "胡荣的个人博客, 分享个人经历",
     "keyword"                   => "开发, 后端, PHP, Java, 架构, 中间件",
+    "motto"                     => "写写代码，吐吐苦水，睡睡懒觉",
     "url"                       => "http://eps.ink",
     "baseurl"                   => "/",
     "cdnUrl"                    => "https://cdn.jsdelivr.net/gh/EPsheep/epsheep.github.io",
+    "timezone"                  => "Asia/Shanghai",
+    "encoding"                  => "utf-8",
 
     // 侧边栏设置
     "sidebar"                   => true,
@@ -42,14 +45,17 @@ $config = [
 
     // 构建设置
     "highlighter"               => "rouge",
-    "permalink"                 => "pretty",
+    "permalink"                 => "/posts/:title",
     "paginate"                  => 10,
     "exclude"                   => [
-        "less",
-        "node_modules",
-        "Gruntfile.js",
+        "build",
+        "css",
+        "img",
+        "js",
+        "CNAME",
+        "LICENSE",
         "package.json",
-        "README.md"
+        "README.md",
     ],
     "anchorjs"                  => true,
 
@@ -62,16 +68,12 @@ $config = [
     "kramdown"                  => [
         "input" => "GFM"
     ],
-
-    // 图片管理
-
-    //
 ];
 
 
-// 调取每日必应壁纸接口
+// 1. 调取每日必应壁纸接口
 try {
-    $client = new Client();
+    $client               = new Client();
     $response             = $client->request('GET', 'http://fly.atlinker.cn/api/bing/cn.php');
     $imageInfo            = json_decode($response->getBody(), true);
     $config['header-img'] = 'https://cn.bing.com' . $imageInfo['images'][0]['url'];
@@ -80,6 +82,22 @@ try {
     echo '调取每日必应壁纸接口失败!' . PHP_EOL;
 }
 
+// 2. 随机生成签名
+$motto           = [
+    "写写代码，吐吐苦水，睡睡懒觉",
+    "非淡泊无以明志，非宁静无以致远",
+    "抛弃时间的人，时间也抛弃他",
+    "合理安排时间，就等于节约时间",
+    "既然我已经踏上这条道路，那么，任何东西都不应妨碍我沿着这条路走下去",
+    "A man's life is short, but it will be too long if he spends it despicably",
+    "No matter how long the night is, the day will come",
+    "十年饮冰，难凉热血",
+    "在等待的日子里，刻苦读书，谦卑做人，养得根深，日后才能枝繁叶茂",
+    "故人笑比中庭树，一日秋风一日疏",
+];
+$config['motto'] = $motto[mt_rand(0, count($motto) - 1)];
+echo '随机生成签名成功!';
 
+// 3. 写入配置文件
 file_put_contents('../_config.yml', yaml_emit($config, YAML_UTF8_ENCODING));
 echo '写入配置文件成功!' . PHP_EOL;
